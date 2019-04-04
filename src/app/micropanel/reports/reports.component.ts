d545c4aa2a61728797cbd6e7516510143a02546a
Mr.MHMirzaei <mr.mhmirzaei@gmail.com>
2019-04-04 16:50:28 +0430

    Tables reports

--- src/app/micropanel/reports/reports.component.ts
+++ src/app/micropanel/reports/reports.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  public list:Object[] = [{
+    listname : 'لیست کاربران ',
+    cols : [{
+      view : 'نام',
+      code : 'firstname'
+    },{
+      view : 'نام خانوادگی',
+      code : 'lastname'
+    }],
+    data : [{
+      lastname : 'یاحقی',
+      firstname : 'محمد جواد',
+    }]
+   },{
+    listname : 'لیست کاربران ',
+    cols : [{
+      view : 'نام',
+      code : 'firstname'
+    },{
+      view : 'نام خانوادگی',
+      code : 'lastname'
+    }],
+    data : [{
+      lastname : 'یاحقی',
+      firstname : 'محمد جواد',
+    }]
+   }];
+   
+  private heads = [];
 
-  ngOnInit() {
+  ngOnInit(){}
+
+  setHidden(item){
+    if(item['hidden'] == null){
+      item['hidden'] = false;
+    } else {
+      item['hidden'] = !item['hidden'];
+    }
   }
 
+  setTH(col, i){
+    let cols: Object[] = this.heads[i];
+    if(cols == null) {
+      this.heads[i] = [];
+    }
+    if(cols.includes(col['code']) == false){
+      this.heads[i].push(col['code']);
+    }
+    return col['view'];
+  }
 }

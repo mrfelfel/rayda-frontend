eddd671cafc20309d36db82e0030e6250f2f8413
Mr.MHMirzaei <mr.mhmirzaei@gmail.com>
2019-04-12 21:45:14 +0430

    Money component header update!

--- src/app/financial/financial.module.ts
+++ src/app/financial/financial.module.ts
@@ -24,6 +24,7 @@ const routes: Routes = [
     material.MatFormFieldModule,
     material.MatButtonModule,
     material.MatIconModule,
+    material.MatToolbarModule,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
